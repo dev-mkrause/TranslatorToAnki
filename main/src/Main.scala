2bfde291604b0ae0d8ac48c96dc61ddf55bc0dd3
Marvin Krause <krause.marvin@proton.me>
2023-05-17 15:00:41 +0200

    add process input function; update regex to include special chars

--- main/src/Main.scala
+++ main/src/Main.scala
@@ -4,7 +4,7 @@ import scala.io.StdIn.readLine
 val splitLowerCase: String => List[String] = _.toLowerCase().split(" ").toList
 
 val removePunctuation: String => String = _.trim
-  .replaceAll("[^a-zA-Z\\s-]", "")
+  .replaceAll("[^\\p{L}-\\s]", "")
   .replaceAll(" +", " ")
 
-@main def main(): Unit = { ??? }
+val processInput: String => List[String] = (input) => splitLowerCase(removePunctuation(input))
--- main/test/src/MainTest.scala
+++ main/test/src/MainTest.scala
@@ -1,7 +1,8 @@
 import munit.FunSuite
 import org.mkrause.TranslatorToAnki._
 
-class MainTests extends FunSuite {
+class InputTests extends FunSuite {
+
   test("Remove punctuation and numbers") {
     assertEquals(removePunctuation("Do I love petting 12 chickens a day? Yes I do! What about you..."), "Do I love petting chickens a day Yes I do What about you")
   }
@@ -13,4 +14,15 @@ class MainTests extends FunSuite {
   test("Take multiple words as input and return them as a lower case List") {
     assertEquals(splitLowerCase("I lOvE PeTtInG cHicKeNs"), List("i", "love", "petting", "chickens"), "Input not list of lower case words")
   }
+
+  test("format input for processing") {
+    val input = "Do I love petting 12 chickens a day? Yes I do! What about you..."
+    assertEquals(processInput(input), List("do", "i", "love", "petting", "chickens", "a", "day", "yes", "i", "do", "what", "about", "you"))
+  }
+
+  test("Keep Special characters, accents and diacritical marks in input") {
+    val input = "Hállö, wørld! Café ànd pâté."
+    assertEquals(processInput(input), List("hállö", "wørld", "café", "ànd", "pâté"))
+  }
+}
 }
